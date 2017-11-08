d60f2f9a42d71e0d07f2f65b2d25e9864813269e
khusika <khusikadhamar@gmail.com>
2017-11-08 20:02:24 +0700

    Add onBackPressed

--- app/src/main/java/com/dev/jobtracker/HomeActivity.java
+++ app/src/main/java/com/dev/jobtracker/HomeActivity.java
@@ -1,6 +1,23 @@
 package com.dev.jobtracker;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
 public class HomeActivity extends BaseActivity {
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        HomeActivity.super.onBackPressed();
+                    }
+                }).create().show();
+    }
 
 
     @Override
